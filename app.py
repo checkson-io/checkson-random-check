11cda5b5adf5538eb96ff8f3b10cd3c3eac63fb9
Stefan Hudelmaier <hudelmaier@gmail.com>
2023-08-09 08:32:30 +0200

    Output message

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import sys
 import random
+import os
+
+checkson_dir = os.environ['CHECKSON_DIR']
+
+
+def write_message(message):
+    with open(f"{checkson_dir}/message", "w") as f:
+        f.write(message)
 
 
 if random.random() > 0.5:
     print("Check successful")
+    write_message("Check successful")
     sys.exit(0)
 else:
     print("Check unsuccessful")
+    write_message("Check unsuccessful")
     sys.exit(1)
