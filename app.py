ce059901be1737d25be9e8dad22867513cc508cd
Stefan Hudelmaier <hudelmaier@gmail.com>
2023-10-03 13:30:42 +0200

    Write multiple attachments

--- app.py
+++ app.py
@@ -10,19 +10,21 @@ def write_message(message):
         f.write(message)
 
 
-def write_attachment(contents):
+def write_attachment(filename, contents):
     os.makedirs(f"{checkson_dir}/attachments", exist_ok=True)
-    with open(f"{checkson_dir}/attachments/example-attachment.txt", "w") as f:
+    with open(f"{checkson_dir}/attachments/{filename}", "w") as f:
         f.write(contents)
 
 
 if random.random() > 0.5:
     print("Check successful")
     write_message("Check successful")
-    write_attachment("This is an example attachment")
+    write_attachment("attachment1.txt", "This is an example attachment")
+    write_attachment("attachment2.txt", "This is an example attachment")
     sys.exit(0)
 else:
     print("Check unsuccessful")
     write_message("Check unsuccessful")
-    write_attachment("This is an example attachment")
+    write_attachment("attachment1.txt", "This is an example attachment")
+    write_attachment("attachment2.txt", "This is an example attachment")
     sys.exit(1)
